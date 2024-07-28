6eaf8bd4d3d44512ebafb718357004eec0c93ada
bashizade <bashizade.official@gmail.com>
2024-07-28 23:08:00 +0330

    fix

--- main/settings.py
+++ main/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'library',
-    'auth'
+    '_auth_'
 ]
 
 MIDDLEWARE = [
--- main/urls.py
+++ main/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('library/', include('library.urls')),
-    path('', include('auth.urls'))
+    path('', include('_auth_.urls'))
 ]
